f6ecc8467b875bcfce5dc5b8b0635fcd77f604e0
Prabhakar Patil <prabhakar@squats.in>
2020-02-04 23:58:48 +0530

    Initial Commit

---
+++ Much Better/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Much Better
+//
+//  Created by squats on 2/4/20.
+//  Copyright © 2020 prabhakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
